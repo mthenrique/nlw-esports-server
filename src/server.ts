735b1a15f2c72f69507789a1902336205f4a296e
Mateus oliveira <mthenriqueo@gmail.com>
2022-09-15 18:33:43 -0300

    Criando as models

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,20 @@ import { Request, Response } from 'express'
 
 const app = express() 
 
-app.get('/ping', (request: Request, response: Response) => {
-  return response.json({ message: 'Retorno do ping' })
+app.get('/games', (request: Request, response: Response) => {
+  return response.json([])
+})
+
+app.post('/ads', (request: Request, response: Response) => {
+  return response.json([])
+})
+
+app.post('/games/:id/ads', (request: Request, response: Response) => {
+  return response.json([])
+})
+
+app.post('/ads/:id/discord', (request: Request, response: Response) => {
+  return response.json([])
 })
 
 app.listen(3333)
